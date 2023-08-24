06aa1b1c196bbeb3a4d0a0a471d4ae708e36e41d
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2023-08-24 15:46:56 +0900

    update box

--- buildScript/lib/core/get_source_env.sh
+++ buildScript/lib/core/get_source_env.sh
@@ -1,5 +1,5 @@
 if [ ! -z $ENV_NB4A ]; then
-  export COMMIT_SING_BOX_EXTRA="1e43a84c4555250318acfca0a2fe5bc4f3370324"
+  export COMMIT_SING_BOX_EXTRA="3c5d4ae9b771f216bc0eeabdf19d840c77e29858"
 fi
 
 if [ ! -z $ENV_SING_BOX_EXTRA ]; then
