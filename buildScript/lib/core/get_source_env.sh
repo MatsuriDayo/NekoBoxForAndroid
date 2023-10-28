2fe829012c19c57039e436e40ee3816e5ec20714
Misaka-blog <misakablog@protonmail.com>
2023-10-28 11:41:03 +0800

    1.2.8

--- buildScript/lib/core/get_source_env.sh
+++ buildScript/lib/core/get_source_env.sh
@@ -1,5 +1,5 @@
 if [ ! -z $ENV_NB4A ]; then
-  export COMMIT_SING_BOX_EXTRA="9965df4ed5f70fdc1d541e97861dff001111e011"
+  export COMMIT_SING_BOX_EXTRA="247a7a4522168aa8b151efc4085809cae819345d"
 fi
 
 if [ ! -z $ENV_SING_BOX_EXTRA ]; then
