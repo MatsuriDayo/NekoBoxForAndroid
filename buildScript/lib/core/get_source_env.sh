d1d1265f0b2a80652bf238899b273fbcee2fc2d2
Misaka-blog <misakablog@protonmail.com>
2023-10-28 18:49:38 +0800

    update box

--- buildScript/lib/core/get_source_env.sh
+++ buildScript/lib/core/get_source_env.sh
@@ -1,5 +1,5 @@
 if [ ! -z $ENV_NB4A ]; then
-  export COMMIT_SING_BOX_EXTRA="247a7a4522168aa8b151efc4085809cae819345d"
+  export COMMIT_SING_BOX_EXTRA="2cc111cc8c059d0654b0a600785f98d93048c67a"
 fi
 
 if [ ! -z $ENV_SING_BOX_EXTRA ]; then
