c584a13e26272884e71223292fe3fcf3ef208809
purofle <purofle@gmail.com>
2023-11-15 00:41:11 +0800

    refactor: update box

--- buildScript/lib/core/get_source_env.sh
+++ buildScript/lib/core/get_source_env.sh
@@ -1,5 +1,5 @@
 if [ ! -z $ENV_NB4A ]; then
-  export COMMIT_SING_BOX_EXTRA="7744cf058d7c2abcb7512a47d7baa9552dbf67c3"
+  export COMMIT_SING_BOX_EXTRA="23f074fcd7938559752d9b3d16d5dcb0ff9851b9"
 fi
 
 if [ ! -z $ENV_SING_BOX_EXTRA ]; then
