81864b523435a1fd4e794d5c1d96673ddea117e1
HystericalDragon <HystericalDragons@proton.me>
2023-12-16 20:23:25 +0800

    refactor(libcore): remove old APIs
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- libcore/io.go
+++ libcore/io.go
@@ -1,12 +1,14 @@
 package libcore
 
 import (
-	"context"
+	"archive/zip"
 	"io"
 	"os"
+        "path/filepath"
 
-	"github.com/codeclysm/extract"
 	"github.com/ulikunitz/xz"
+	"github.com/sagernet/sing/common"
+        E "github.com/sagernet/sing/common/exceptions"
 )
 
 func Unxz(archive string, path string) error {
@@ -30,11 +32,47 @@ func Unxz(archive string, path string) error {
 }
 
 func Unzip(archive string, path string) error {
-	i, err := os.Open(archive)
+	r, err := zip.OpenReader(archive)
 	if err != nil {
 		return err
 	}
-	defer i.Close()
-	err = extract.Zip(context.Background(), i, path, nil)
-	return err
+	defer r.Close()
+
+	err = os.MkdirAll(path, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range r.File {
+		filePath := filepath.Join(path, file.Name)
+
+		if file.FileInfo().IsDir() {
+			err = os.MkdirAll(filePath, os.ModePerm)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		newFile, err := os.Create(filePath)
+		if err != nil {
+			return err
+		}
+
+		zipFile, err := file.Open()
+		if err != nil {
+			newFile.Close()
+			return err
+		}
+
+		var errs error
+		_, err = io.Copy(newFile, zipFile)
+		errs = E.Errors(errs, err)
+		errs = E.Errors(errs, common.Close(zipFile, newFile))
+		if errs != nil {
+			return errs
+		}
+	}
+
+	return nil
 }
