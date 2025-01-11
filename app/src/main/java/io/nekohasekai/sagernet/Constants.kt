dd95867967d09bc2e34278707c3fcfa9a39f96ef
starifly <2003879+starifly@users.noreply.github.com>
2025-01-11 13:35:39 +0800

    更换默认测试链接

--- app/src/main/java/io/nekohasekai/sagernet/Constants.kt
+++ app/src/main/java/io/nekohasekai/sagernet/Constants.kt
@@ -1,6 +1,6 @@
 package io.nekohasekai.sagernet
 
-const val CONNECTION_TEST_URL = "http://cp.cloudflare.com/"
+const val CONNECTION_TEST_URL = "http://www.gstatic.com/generate_204"
 
 object Key {
 
